75c827077d9387accadc1f2369f3befb49ea1541
Aditya Pahane <adityapahane@gmail.com>
2025-01-31 23:22:45 +0530

    Access Modifiers

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 "use strict";
-// var a = 12
-// var b = 13
-// function abcd(){
-//     let x = 12
-//     let y = 13
+// // enumrations
+//     enum UserRoles{
+//     ADMIN = "admin",
+//     GUEST = "guest",
+//     SUPER_ADMIN = "super_admin"
 // }
-var UserRoles;
-(function (UserRoles) {
-    UserRoles["ADMIN"] = "admin";
-    UserRoles["GUEST"] = "guest";
-    UserRoles["SUPER_ADMIN"] = "super_admin";
-})(UserRoles || (UserRoles = {}));
+// //
+// let a: number;
+// a = 12
+// //void
+// function abcde(): number{
+//     let num1 = 1
+//     let num2 = 1
+//     let sum = num1 + num2
+//     return sum
+// }
+// // never
+// function abc(): never{
+//     while(true){
+//     }
+// }
+// abc()
+// console.log("hello");
+//
+// interface User{
+//     name: string,
+//     email: string,
+//     password: string,
+//     gender?: string
+// }
+// function abcd (obj: User){
+// }
+// abcd()
+//Classes making
+class Device {
+    constructor() {
+        this.name = "LG";
+        this.price = "20000";
+        this.category = "Tv";
+    }
+}
+let d1 = new Device();
+// constructor
+class BottleMaker {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+let b1 = new BottleMaker("Milton", 1200);
--- app.ts
+++ app.ts
@@ -1,7 +1,59 @@
 
-enum UserRoles{
-    ADMIN = "admin",
-    GUEST = "guest",
-    SUPER_ADMIN = "super_admin"
-}
+// // enumrations
+//     enum UserRoles{
+//     ADMIN = "admin",
+//     GUEST = "guest",
+//     SUPER_ADMIN = "super_admin"
+// }
+// //
+// let a: number;
+// a = 12
 
+// //void
+// function abcde(): number{
+//     let num1 = 1
+//     let num2 = 1
+//     let sum = num1 + num2
+//     return sum
+// }
+// // never
+// function abc(): never{
+//     while(true){
+
+//     }
+// }
+// abc()
+// console.log("hello");
+
+
+
+//
+
+// interface User{
+//     name: string,
+//     email: string,
+//     password: string,
+//     gender?: string
+// }
+// function abcd (obj: User){
+
+// }
+// abcd()
+
+
+//Classes making
+// class Device{
+//     name = "LG"
+//     price = "20000"
+//     category = "Tv"
+// }
+// let d1 = new Device()
+
+// // constructor
+// class BottleMaker{
+//     constructor(public name:string, public price:number){
+//     }
+// }
+// let b1 = new BottleMaker("Milton", 1200)
+
+//Accces modifiers
